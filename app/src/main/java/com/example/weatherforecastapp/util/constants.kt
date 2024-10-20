3ddcf5e27bb73293b6b1d9f687fcba4c1018a53e
Codehub <adityapersonal543@gmail.com>
2024-10-20 13:21:54 +0530

    Stop tracking constant

--- app/src/main/java/com/example/weatherforecastapp/util/constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weatherforecastapp.util
-
-object constants {
-    const val BASE_URL ="https://api.openweathermap.org/"
-    const val API_KEY ="fbe592b2ce6e22ccdc67bc5ac55f167f"
-}
